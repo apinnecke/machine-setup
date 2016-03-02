a67cbee6d536e8ecd4951e7fb94612560a3ce662
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2016-03-02 09:54:35 +0100

    Added screenhero

--- osx-setup.sh
+++ osx-setup.sh
@@ -14,7 +14,7 @@ brew install \
 brew cask install \
 	iterm2 1password chitchat karabiner seil google-chrome firefox opera atom \
 	caffeine evernote flycut lastfm skype slack spotify spotify-notifications \
-	sublime-text vagrant vagrant-manager virtualbox vlc viscosity
+	sublime-text vagrant vagrant-manager virtualbox vlc viscosity screenhero
 
 alias git="hub"
 
