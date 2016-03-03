415cb96cd866b69673388502a63919398e089a8d
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2016-03-03 22:55:37 +0100

    Added hyperdock

--- osx-setup.sh
+++ osx-setup.sh
@@ -16,7 +16,9 @@ brew cask install \
 	iterm2 1password chitchat karabiner seil google-chrome firefox opera atom \
 	caffeine evernote flycut lastfm skype slack spotify spotify-notifications \
 	sublime-text vagrant vagrant-manager virtualbox vlc viscosity screenhero \
-	java
+	java hyperdock
+
+gem install git-up
 
 sudo gem install git-up
 echo 'require_confirmation = False' >> ~/.config/thefuck/settings.py
@@ -36,11 +38,8 @@ git clone apinnecke/dotfiles ~/dotfiles
 cd ~/dotfiles && git submodule update --init
 bash ~/dotfiles/linkfiles.sh
 
-# create src dir and
-sudo mkdir -p /src/apinnecke
+# create src dir
+sudo mkdir -p /src
 sudo chown -R apinnecke:admin /src
 
-# install osx-setup
-git clone apinnecke/osx-setup /src/apinnecke/osx-setup
-brew install $(cat /src/apinnecke/osx-setup/brew-packages)
-brew cask install $(cat /src/apinnecke/osx-setup/brew-cask-packages)
+sudo npm install -g gulp-cli pm2
